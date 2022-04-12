5c41d7997c48715d59c27c5c0ec071839e495489
Patrick-Star-CN <1592810043@qq.com>
2022-04-12 23:02:11 +0800

    fix(mysql): fix cant run sql file

--- backend/src/main/resources/confession_wall.sql
+++ backend/src/main/resources/confession_wall.sql
@@ -23,10 +23,10 @@ SET FOREIGN_KEY_CHECKS = 0;
 DROP TABLE IF EXISTS `userdata`;
 CREATE TABLE `userdata`  (
   `uid` int(0) NOT NULL AUTO_INCREMENT,
-  `username` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `username` varchar(255) CHARACTER SET utf8mb4 NOT NULL,
+  `password` varchar(255) CHARACTER SET utf8mb4 NOT NULL,
   PRIMARY KEY (`uid`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 5 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 5 CHARACTER SET = utf8mb4 ROW_FORMAT = Dynamic;
 
 SET FOREIGN_KEY_CHECKS = 1;
 
@@ -37,14 +37,14 @@ DROP TABLE IF EXISTS `confessiondata`;
 CREATE TABLE `confessiondata`  (
   `id` int(0) NOT NULL AUTO_INCREMENT,
   `uid` int(0) NOT NULL,
-  `username` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `content` varchar(10000) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `username` varchar(255) CHARACTER SET utf8mb4 NOT NULL,
+  `content` varchar(10000) CHARACTER SET utf8mb4 NOT NULL,
   `date` date NOT NULL,
-  `target` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `target` varchar(255) CHARACTER SET utf8mb4 NOT NULL,
   PRIMARY KEY (`id`) USING BTREE,
   INDEX `KF_uid`(`uid`) USING BTREE,
   CONSTRAINT `KF_uid` FOREIGN KEY (`uid`) REFERENCES `userdata` (`uid`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE = InnoDB AUTO_INCREMENT = 4 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 4 CHARACTER SET = utf8mb4 ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for commentdata
@@ -54,11 +54,11 @@ CREATE TABLE `commentdata`  (
   `id` int(0) NOT NULL AUTO_INCREMENT,
   `uid` int(0) NOT NULL,
   `confessionid` int(0) NOT NULL,
-  `content` varchar(10000) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `content` varchar(10000) CHARACTER SET utf8mb4 NOT NULL,
   `date` date NOT NULL,
   PRIMARY KEY (`id`) USING BTREE,
   INDEX `FK_uid`(`uid`) USING BTREE,
   INDEX `FK_confessionid`(`confessionid`) USING BTREE,
   CONSTRAINT `FK_confessionid` FOREIGN KEY (`confessionid`) REFERENCES `confessiondata` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `FK_uid` FOREIGN KEY (`uid`) REFERENCES `userdata` (`uid`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 ROW_FORMAT = Dynamic;
